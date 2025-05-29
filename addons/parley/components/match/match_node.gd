378bafe26902574f71d360dc4a6d18bf9272e073
Jonny Green <jonny.green@bisterixstudio.com>
2025-05-29 23:28:15 +0100

    Style bugfixes and fixup missing connection on dialogue option nodes

--- addons/parley/components/match/match_node.gd
+++ addons/parley/components/match/match_node.gd
@@ -2,20 +2,25 @@
 # TODO: prefix with Parley
 class_name MatchNode extends ParleyGraphNode
 
+
+#region DEFS
 const case_label_scene: PackedScene = preload("./case_label.tscn")
 
-@export var description: String = "": set = _on_set_description
-@export var fact_name: String = "": set = _on_set_fact_name
-@export var cases: Array[Variant] = []: set = _on_cases_changed
+
+@export var description: String = "": set = _set_description
+@export var fact_name: String = "": set = _set_fact_name
+@export var cases: Array[Variant] = []: set = _set_cases
+
 
 @onready var description_label: Label = %MatchDescription
 @onready var fact_label: Label = %Fact
 
-static var start_slot: int = 3
 
-#############
-# Lifecycle #
-#############
+static var start_slot: int = 3
+#endregion
+
+
+#region LIFECYCLE
 func _ready() -> void:
 	setup(DialogueAst.Type.MATCH)
 	clear_all_slots()
@@ -26,32 +31,22 @@ func _ready() -> void:
 	_render_description()
 	_render_fact_name()
 	_render_cases()
+#endregion
 
-# Get around what appears to be a bug where the slots are not correctly positioned
-# by moving a node back and forth to the original position
-func _refresh_slot_positions() -> void:
-	position.x += 1
-	await get_tree().create_timer(0.01).timeout
-	position.x -= 1
 
-func _render_description() -> void:
-	if is_node_ready():
-		description_label.text = description
-
-func _render_fact_name() -> void:
-	if is_node_ready():
-		fact_label.text = fact_name
-
-func _on_set_description(new_description: String) -> void:
+#region SETTERS
+func _set_description(new_description: String) -> void:
 	description = new_description
 	_render_description()
 
 
-func _on_set_fact_name(new_fact_name: String) -> void:
+func _set_fact_name(new_fact_name: String) -> void:
 	fact_name = new_fact_name
 	_render_fact_name()
+#endregion
 
 
+#region RENDERERS
 # TODO: we may have rendering issues with the edges are lost upon
 # slot changes. For now, let's just re-render the slots and then handler
 # at a later date.
@@ -77,21 +72,45 @@ func _render_cases() -> void:
 		_render_footer(index)
 		_refresh_slot_positions()
 
+
 func _render_footer(index: int) -> void:
 	var footer: MarginContainer = MarginContainer.new()
 	footer.custom_minimum_size = Vector2(0, 15)
 	add_child(footer)
 	set_slot(index, false, 0, Color.CHARTREUSE, false, 0, Color.CHARTREUSE)
-	
 
-func _on_cases_changed(new_cases: Array[Variant]) -> void:
+
+# Get around what appears to be a bug where the slots are not correctly positioned
+# by moving a node back and forth to the original position
+func _refresh_slot_positions() -> void:
+	position.x += 1
+	await get_tree().create_timer(0.01).timeout
+	position.x -= 1
+
+
+func _render_description() -> void:
+	if description_label:
+		description_label.text = description
+
+
+func _render_fact_name() -> void:
+	if fact_label:
+		fact_label.text = fact_name
+#endregion
+
+
+#region SIGNALS
+func _set_cases(new_cases: Array[Variant]) -> void:
 	cases = new_cases
 	_render_cases()
+#endregion
+
 
 #region OVERRIDES
 ## Select from slot by changing to blue colour
 func select_from_slot(from_slot: int) -> void:
 	set_slot_color_right(from_slot + start_slot, Color.CORNFLOWER_BLUE)
+
 
 ## Unselect from slot by returning back to original colour
 func unselect_from_slot(from_slot: int) -> void:
